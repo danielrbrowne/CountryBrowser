c196368de6d09b11f135cf55bdd02f684ca8f8a0
Daniel Browne <daniel.browne@pusher.com>
2021-06-08 15:31:02 +0100

    Initial Commit

---
+++ CountryBrowser/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CountryBrowser
+//
+//  Created by Daniel Browne on 08/06/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CountryBrowser/CountryBrowserApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountryBrowserApp.swift
+//  CountryBrowser
+//
+//  Created by Daniel Browne on 08/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct CountryBrowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
